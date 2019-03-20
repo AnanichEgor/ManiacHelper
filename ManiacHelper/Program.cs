a7971ae61739b2194651edc41a4a9c9efb1f21ef
Egor Ananich <aevbox@gmail.com>
2019-03-20 21:30:53 +0300

    edit path to resource file

--- ManiacHelper/Program.cs
+++ ManiacHelper/Program.cs
@@ -6,7 +6,7 @@ namespace ManiacHelper
     {
         static void Main(string[] args)
         {
-            var resource = new Resources(@"C:\Users\Yahor_Ananich\source\repos\ManiacHelper\ManiacHelper\Resources\GK.txt");
+            var resource = new Resources(@"..\..\..\Resources\GK.txt");
             var testPhrase = new Engine.ManiacHelper(resource.ArrayDictionary);
             Console.WriteLine(testPhrase.PhraseCheck("Национальный реестр правовых актов ребенока"));
             Console.ReadKey();
